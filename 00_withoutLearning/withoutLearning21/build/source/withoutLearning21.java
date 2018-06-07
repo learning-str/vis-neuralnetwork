80ce60944304076b06fa8ca9527739ee1b2928c6
SatoruOsawa <osawa@takram.com>
2018-06-07 19:06:08 +0900

    using controlP5

--- 00_withoutLearning/withoutLearning21/build/source/withoutLearning21.java
+++ 00_withoutLearning/withoutLearning21/build/source/withoutLearning21.java
@@ -3,6 +3,8 @@ import processing.data.*;
 import processing.event.*; 
 import processing.opengl.*; 
 
+import controlP5.*; 
+
 import java.util.HashMap; 
 import java.util.ArrayList; 
 import java.io.File; 
@@ -38,24 +40,32 @@ public void setup() {
   NEURON_0_0 = new Neuron(0.3f, -2.2f, -1.4f);
   NEURON_0_1 = new Neuron(-0.4f, -0.1f, 0.2f);
   NEURON_1_0 = new Neuron(-0.2f, 0.5f, -0.2f);
+
+  setupUi();
+}
+
+public void update() {
+  updateParameters();
 }
 
 public void draw() {
-  translate(width / 2, height / 2);
-  for (int i = 0; i < 10000; i++) {
-    float x1 = random(-MAX, MAX);
-    float x2 = random(-MAX, MAX);
-    noStroke();
-    drawNeuron(x1, x2, 2);
-  }
-  drawAxis();
-  stroke(150);
-  strokeWeight(1);
-  drawNeuron(0, 0, 15);
-  drawNeuron(0, 1, 15);
-  drawNeuron(1, 0, 15);
-  drawNeuron(1, 1, 15);
-  drawParameters();
+  update();
+  pushMatrix(); {
+    translate(width / 2, height / 2);
+    for (int i = 0; i < 10000; i++) {
+      float x1 = random(-MAX, MAX);
+      float x2 = random(-MAX, MAX);
+      noStroke();
+      drawNeuron(x1, x2, 4);
+    }
+    drawAxis();
+    stroke(150);
+    strokeWeight(1);
+    drawNeuron(0, 0, 15);
+    drawNeuron(0, 1, 15);
+    drawNeuron(1, 0, 15);
+    drawNeuron(1, 1, 15);
+  } popMatrix();
 }
 
 public void drawNeuron(float x1, float x2, float size) {
@@ -70,108 +80,22 @@ public void drawNeuron(float x1, float x2, float size) {
   ellipse(x1 * SCALE, -x2 * SCALE, size, size);
 }
 
-public void drawAxis() {
-  stroke(150);
-  line(-width / 2, 0, width / 2, 0);
-  line(0, -height / 2, 0, height / 2);
-  textFont(HIRAGINO20);
-  textSize(20);
-  fill(100);
-  text("x1", width / 2 - 30, -5);
-  text("x2", 5, -height / 2 + 30);
-}
-
-public void drawParameters() {
-  translate(-width / 2, -height / 2);
-  textFont(HIRAGINO10);
-  textSize(10);
-  fill(100);
-  text("P00 weight1: " + NEURON_0_0.weight1(), 5, 20);
-  text("P00 weight2: " + NEURON_0_0.weight2(), 5, 35);
-  text("P00 bias: " + NEURON_0_0.bias(), 5, 50);
-  text("P01 weight1: " + NEURON_0_1.weight1(), 5, 65);
-  text("P01 weight2: " + NEURON_0_1.weight2(), 5, 80);
-  text("P01 bias: " + NEURON_0_1.bias(), 5, 95);
-  text("P10 weight1: " + NEURON_1_0.weight1(), 5, 110);
-  text("P10 weight2: " + NEURON_1_0.weight2(), 5, 125);
-  text("P10 bias: " + NEURON_1_0.bias(), 5, 140);
-  text("mouseX assign: " + MOUSE_X_ASSIGN, 5, 160);
-  text("mouseY assign: " + MOUSE_Y_ASSIGN, 5, 175);
-}
-
-public void mouseMoved() {
-  switch (MOUSE_X_ASSIGN) {
-    case "P00 weight1":
-      NEURON_0_0.weight1(5 * (mouseX - width / 2) / PApplet.parseFloat(width));
-      break;
-    case "P01 weight1":
-      NEURON_0_1.weight1(5 * (mouseX - width / 2) / PApplet.parseFloat(width));
-      break;
-    case "P10 weight1":
-      NEURON_1_0.weight1(5 * (mouseX - width / 2) / PApplet.parseFloat(width));
-      break;
-    case "P00 bias":
-      NEURON_0_0.bias(5 * (mouseX - width / 2) / PApplet.parseFloat(width));
-      break;
-    case "P01 bias":
-      NEURON_0_1.bias(5 * (mouseX - width / 2) / PApplet.parseFloat(width));
-      break;
-    case "P10 bias":
-      NEURON_1_0.bias(5 * (mouseX - width / 2) / PApplet.parseFloat(width));
-      break;
-    default:
-      break;
-  }
-  switch (MOUSE_Y_ASSIGN) {
-    case "P00 weight2":
-      NEURON_0_0.weight2(-5 * (mouseY - height / 2) / PApplet.parseFloat(height));
-      break;
-    case "P01 weight2":
-      NEURON_0_1.weight2(-5 * (mouseY - height / 2) / PApplet.parseFloat(height));
-      break;
-    case "P10 weight2":
-      NEURON_1_0.weight2(-5 * (mouseY - height / 2) / PApplet.parseFloat(height));
-      break;
-    default:
-      break;
-  }
+public void updateParameters() {
+  NEURON_0_0.weight1(CP5.getController("P00_WEIGHT1").getValue());
+  NEURON_0_0.weight2(CP5.getController("P00_WEIGHT2").getValue());
+  NEURON_0_0.bias(CP5.getController("P00_BIAS").getValue());
+  NEURON_0_1.weight1(CP5.getController("P01_WEIGHT1").getValue());
+  NEURON_0_1.weight2(CP5.getController("P01_WEIGHT2").getValue());
+  NEURON_0_1.bias(CP5.getController("P01_BIAS").getValue());
+  NEURON_1_0.weight1(CP5.getController("P10_WEIGHT1").getValue());
+  NEURON_1_0.weight2(CP5.getController("P10_WEIGHT2").getValue());
+  NEURON_1_0.bias(CP5.getController("P10_BIAS").getValue());
 }
 
 public void keyPressed() {
   switch (key) {
     case ' ':
       background(255);
-      break;
-    case 'c':
-      if (MOUSE_MODE == 0) {
-        MOUSE_X_ASSIGN = "P00 weight1";
-        MOUSE_Y_ASSIGN = "P00 weight2";
-        MOUSE_MODE = 1;
-      } else if (MOUSE_MODE == 1) {
-        MOUSE_X_ASSIGN = "P00 bias";
-        MOUSE_Y_ASSIGN = "P00 undefined";
-        MOUSE_MODE = 2;
-      } else if (MOUSE_MODE == 2) {
-        MOUSE_X_ASSIGN = "P01 weight1";
-        MOUSE_Y_ASSIGN = "P01 weight2";
-        MOUSE_MODE = 3;
-      } else if (MOUSE_MODE == 3) {
-        MOUSE_X_ASSIGN = "P01 bias";
-        MOUSE_Y_ASSIGN = "P01 undefined";
-        MOUSE_MODE = 4;
-      } else if (MOUSE_MODE == 4) {
-        MOUSE_X_ASSIGN = "P10 weight1";
-        MOUSE_Y_ASSIGN = "P10 weight2";
-        MOUSE_MODE = 5;
-      } else if (MOUSE_MODE == 5) {
-        MOUSE_X_ASSIGN = "P10 bias";
-        MOUSE_Y_ASSIGN = "P10 undefined";
-        MOUSE_MODE = 6;
-      } else if (MOUSE_MODE == 6) {
-        MOUSE_X_ASSIGN = "P00 undefined";
-        MOUSE_Y_ASSIGN = "P00 undefined";
-        MOUSE_MODE = 0;
-      }
       break;
     default:
       break;
@@ -202,7 +126,100 @@ class Neuron {
   public float weight2() { return weight2; }
   public float bias() { return bias; }
 }
-  public void settings() {  size(500, 500, P2D);  smooth(); }
+
+
+ControlP5 CP5;
+
+int sliderWidth = 100;
+int sliderHeight = 15;
+int rangeMin = -10;
+int rangeMax = 10;
+
+public void setupUi() {
+  int sliderPositionY = 10;
+  CP5 = new ControlP5(this);
+  CP5.setColorCaptionLabel(color(100));
+  CP5.addSlider("P00_WEIGHT1")
+    .setLabel("P00 weight1")
+    .setPosition(10, sliderPositionY)
+    .setSize(sliderWidth, sliderHeight)
+    .setRange(rangeMin, rangeMax);
+  sliderPositionY += 20;
+  CP5.addSlider("P00_WEIGHT2")
+    .setLabel("P00 weight2")
+    .setPosition(10, sliderPositionY)
+    .setSize(sliderWidth, sliderHeight)
+    .setRange(rangeMin, rangeMax);
+  sliderPositionY += 20;
+  CP5.addSlider("P00_BIAS")
+    .setLabel("P00 bias")
+    .setPosition(10, sliderPositionY)
+    .setSize(sliderWidth, sliderHeight)
+    .setRange(rangeMin, rangeMax);
+  sliderPositionY += 20;
+  CP5.addSlider("P01_WEIGHT1")
+    .setLabel("P01 weight1")
+    .setPosition(10, sliderPositionY)
+    .setSize(sliderWidth, sliderHeight)
+    .setRange(rangeMin, rangeMax);
+  sliderPositionY += 20;
+  CP5.addSlider("P01_WEIGHT2")
+    .setLabel("P01 weight2")
+    .setPosition(10, sliderPositionY)
+    .setSize(sliderWidth, sliderHeight)
+    .setRange(rangeMin, rangeMax);
+  sliderPositionY += 20;
+  CP5.addSlider("P01_BIAS")
+    .setLabel("P01 bias")
+    .setPosition(10, sliderPositionY)
+    .setSize(sliderWidth, sliderHeight)
+    .setRange(rangeMin, rangeMax);
+  sliderPositionY += 20;
+  CP5.addSlider("P10_WEIGHT1")
+    .setLabel("P10 weight1")
+    .setPosition(10, sliderPositionY)
+    .setSize(sliderWidth, sliderHeight)
+    .setRange(rangeMin, rangeMax);
+  sliderPositionY += 20;
+  CP5.addSlider("P10_WEIGHT2")
+    .setLabel("P10 weight2")
+    .setPosition(10, sliderPositionY)
+    .setSize(sliderWidth, sliderHeight)
+    .setRange(rangeMin, rangeMax);
+  sliderPositionY += 20;
+  CP5.addSlider("P10_BIAS")
+    .setLabel("P10 bias")
+    .setPosition(10, sliderPositionY)
+    .setSize(sliderWidth, sliderHeight)
+    .setRange(rangeMin, rangeMax);
+  sliderPositionY += 20;
+
+  setValue();
+}
+
+public void setValue() {
+  CP5.getController("P00_WEIGHT1").setValue(NEURON_0_0.weight1());
+  CP5.getController("P00_WEIGHT2").setValue(NEURON_0_0.weight2());
+  CP5.getController("P00_BIAS").setValue(NEURON_0_0.bias());
+  CP5.getController("P01_WEIGHT1").setValue(NEURON_0_1.weight1());
+  CP5.getController("P01_WEIGHT2").setValue(NEURON_0_1.weight2());
+  CP5.getController("P01_BIAS").setValue(NEURON_0_1.bias());
+  CP5.getController("P10_WEIGHT1").setValue(NEURON_1_0.weight1());
+  CP5.getController("P10_WEIGHT2").setValue(NEURON_1_0.weight2());
+  CP5.getController("P10_BIAS").setValue(NEURON_1_0.bias());
+}
+
+public void drawAxis() {
+  stroke(150);
+  line(-width / 2, 0, width / 2, 0);
+  line(0, -height / 2, 0, height / 2);
+  textFont(HIRAGINO20);
+  textSize(20);
+  fill(100);
+  text("x1", width / 2 - 30, -5);
+  text("x2", 5, -height / 2 + 30);
+}
+  public void settings() {  size(1000, 1000, P2D);  smooth(); }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "withoutLearning21" };
     if (passedArgs != null) {
--- 00_withoutLearning/withoutLearning21/withoutLearning21.pde
+++ 00_withoutLearning/withoutLearning21/withoutLearning21.pde
@@ -12,7 +12,7 @@ String MOUSE_Y_ASSIGN = "P00 undefined";
 int MOUSE_MODE = 0;
 
 void setup() {
-  size(500, 500, P2D);
+  size(1000, 1000, P2D);
   SCALE = width / MAX;
   background(255);
   smooth();
@@ -22,24 +22,32 @@ void setup() {
   NEURON_0_0 = new Neuron(0.3, -2.2, -1.4);
   NEURON_0_1 = new Neuron(-0.4, -0.1, 0.2);
   NEURON_1_0 = new Neuron(-0.2, 0.5, -0.2);
+
+  setupUi();
+}
+
+void update() {
+  updateParameters();
 }
 
 void draw() {
-  translate(width / 2, height / 2);
-  for (int i = 0; i < 10000; i++) {
-    float x1 = random(-MAX, MAX);
-    float x2 = random(-MAX, MAX);
-    noStroke();
-    drawNeuron(x1, x2, 2);
-  }
-  drawAxis();
-  stroke(150);
-  strokeWeight(1);
-  drawNeuron(0, 0, 15);
-  drawNeuron(0, 1, 15);
-  drawNeuron(1, 0, 15);
-  drawNeuron(1, 1, 15);
-  drawParameters();
+  update();
+  pushMatrix(); {
+    translate(width / 2, height / 2);
+    for (int i = 0; i < 10000; i++) {
+      float x1 = random(-MAX, MAX);
+      float x2 = random(-MAX, MAX);
+      noStroke();
+      drawNeuron(x1, x2, 4);
+    }
+    drawAxis();
+    stroke(150);
+    strokeWeight(1);
+    drawNeuron(0, 0, 15);
+    drawNeuron(0, 1, 15);
+    drawNeuron(1, 0, 15);
+    drawNeuron(1, 1, 15);
+  } popMatrix();
 }
 
 void drawNeuron(float x1, float x2, float size) {
@@ -54,108 +62,22 @@ void drawNeuron(float x1, float x2, float size) {
   ellipse(x1 * SCALE, -x2 * SCALE, size, size);
 }
 
-void drawAxis() {
-  stroke(150);
-  line(-width / 2, 0, width / 2, 0);
-  line(0, -height / 2, 0, height / 2);
-  textFont(HIRAGINO20);
-  textSize(20);
-  fill(100);
-  text("x1", width / 2 - 30, -5);
-  text("x2", 5, -height / 2 + 30);
-}
-
-void drawParameters() {
-  translate(-width / 2, -height / 2);
-  textFont(HIRAGINO10);
-  textSize(10);
-  fill(100);
-  text("P00 weight1: " + NEURON_0_0.weight1(), 5, 20);
-  text("P00 weight2: " + NEURON_0_0.weight2(), 5, 35);
-  text("P00 bias: " + NEURON_0_0.bias(), 5, 50);
-  text("P01 weight1: " + NEURON_0_1.weight1(), 5, 65);
-  text("P01 weight2: " + NEURON_0_1.weight2(), 5, 80);
-  text("P01 bias: " + NEURON_0_1.bias(), 5, 95);
-  text("P10 weight1: " + NEURON_1_0.weight1(), 5, 110);
-  text("P10 weight2: " + NEURON_1_0.weight2(), 5, 125);
-  text("P10 bias: " + NEURON_1_0.bias(), 5, 140);
-  text("mouseX assign: " + MOUSE_X_ASSIGN, 5, 160);
-  text("mouseY assign: " + MOUSE_Y_ASSIGN, 5, 175);
-}
-
-void mouseMoved() {
-  switch (MOUSE_X_ASSIGN) {
-    case "P00 weight1":
-      NEURON_0_0.weight1(5 * (mouseX - width / 2) / float(width));
-      break;
-    case "P01 weight1":
-      NEURON_0_1.weight1(5 * (mouseX - width / 2) / float(width));
-      break;
-    case "P10 weight1":
-      NEURON_1_0.weight1(5 * (mouseX - width / 2) / float(width));
-      break;
-    case "P00 bias":
-      NEURON_0_0.bias(5 * (mouseX - width / 2) / float(width));
-      break;
-    case "P01 bias":
-      NEURON_0_1.bias(5 * (mouseX - width / 2) / float(width));
-      break;
-    case "P10 bias":
-      NEURON_1_0.bias(5 * (mouseX - width / 2) / float(width));
-      break;
-    default:
-      break;
-  }
-  switch (MOUSE_Y_ASSIGN) {
-    case "P00 weight2":
-      NEURON_0_0.weight2(-5 * (mouseY - height / 2) / float(height));
-      break;
-    case "P01 weight2":
-      NEURON_0_1.weight2(-5 * (mouseY - height / 2) / float(height));
-      break;
-    case "P10 weight2":
-      NEURON_1_0.weight2(-5 * (mouseY - height / 2) / float(height));
-      break;
-    default:
-      break;
-  }
+void updateParameters() {
+  NEURON_0_0.weight1(CP5.getController("P00_WEIGHT1").getValue());
+  NEURON_0_0.weight2(CP5.getController("P00_WEIGHT2").getValue());
+  NEURON_0_0.bias(CP5.getController("P00_BIAS").getValue());
+  NEURON_0_1.weight1(CP5.getController("P01_WEIGHT1").getValue());
+  NEURON_0_1.weight2(CP5.getController("P01_WEIGHT2").getValue());
+  NEURON_0_1.bias(CP5.getController("P01_BIAS").getValue());
+  NEURON_1_0.weight1(CP5.getController("P10_WEIGHT1").getValue());
+  NEURON_1_0.weight2(CP5.getController("P10_WEIGHT2").getValue());
+  NEURON_1_0.bias(CP5.getController("P10_BIAS").getValue());
 }
 
 void keyPressed() {
   switch (key) {
     case ' ':
       background(255);
-      break;
-    case 'c':
-      if (MOUSE_MODE == 0) {
-        MOUSE_X_ASSIGN = "P00 weight1";
-        MOUSE_Y_ASSIGN = "P00 weight2";
-        MOUSE_MODE = 1;
-      } else if (MOUSE_MODE == 1) {
-        MOUSE_X_ASSIGN = "P00 bias";
-        MOUSE_Y_ASSIGN = "P00 undefined";
-        MOUSE_MODE = 2;
-      } else if (MOUSE_MODE == 2) {
-        MOUSE_X_ASSIGN = "P01 weight1";
-        MOUSE_Y_ASSIGN = "P01 weight2";
-        MOUSE_MODE = 3;
-      } else if (MOUSE_MODE == 3) {
-        MOUSE_X_ASSIGN = "P01 bias";
-        MOUSE_Y_ASSIGN = "P01 undefined";
-        MOUSE_MODE = 4;
-      } else if (MOUSE_MODE == 4) {
-        MOUSE_X_ASSIGN = "P10 weight1";
-        MOUSE_Y_ASSIGN = "P10 weight2";
-        MOUSE_MODE = 5;
-      } else if (MOUSE_MODE == 5) {
-        MOUSE_X_ASSIGN = "P10 bias";
-        MOUSE_Y_ASSIGN = "P10 undefined";
-        MOUSE_MODE = 6;
-      } else if (MOUSE_MODE == 6) {
-        MOUSE_X_ASSIGN = "P00 undefined";
-        MOUSE_Y_ASSIGN = "P00 undefined";
-        MOUSE_MODE = 0;
-      }
       break;
     default:
       break;
